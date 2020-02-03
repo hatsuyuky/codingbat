455cee743c99d71e0c48cc005cc2acb85a85abad
longtruong <longtruongcvd@gmail.com>
2020-02-03 20:58:28 +0700

    longtime no see

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -333,5 +333,59 @@ maxTriple([5, 2, 3]) → 5*/
          max=nums[2];
      return max;
  }
+/*
+*
+Given an int array of any length, return a new array of its first 2 elements. If the array is smaller than length 2, use whatever elements are present.
+
+
+frontPiece([1, 2, 3]) → [1, 2]
+frontPiece([1, 2]) → [1, 2]
+frontPiece([1]) → [1]*/
+int [] frontPiece(int nums[]){
+    if(nums.length<=2)
+        return nums;
+    nums = new int[]{nums[0],nums[1]};
+    return nums;
+}
+/*
+*
+Return the number of even ints in the given array. Note: the % "mod" operator computes the remainder, e.g. 5 % 2 is 1.
+
+
+countEvens([2, 1, 2, 3, 4]) → 3
+countEvens([2, 2, 0]) → 3
+countEvens([1, 3, 5]) → 0*/
+
+
+public int countEvens(int[]nums){
+    int event=0;
+    for (int i =0;i<nums.length;i++){
+        if(nums[i]%2==0)
+            event++;
+    }
+    return event;
+}
+/*
+*
+Given an array length 1 or more of ints, return the difference between the largest and smallest values in the array.
+*  Note: the built-in Math.min(v1, v2) and Math.max(v1, v2) methods return the smaller or larger of two values.
+
+
+bigDiff([10, 3, 5, 6]) → 7
+bigDiff([7, 2, 10, 9]) → 8
+bigDiff([2, 10, 7, 2]) → 8*/
+public int bigDiff(int nums[]){
+
+    int max = nums[0];
+    int min = nums[0];
+    for(int i = 1; i < nums.length; i++)
+    {
+        if(nums[i] > max)
+            max = nums[i];
+        else if(nums[i] < min)
+            min = nums[i];
+    }
+    return (max-min);
+}
 
 }
